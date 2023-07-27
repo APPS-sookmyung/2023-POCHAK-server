8380186a1b3e0c9ac50510c9d98a5e9b4c069950
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2023-07-28 00:24:01 +0900

    #6 Feat : request dto 설정

--- pochak/src/main/java/com/apps/pochak/post/dto/PostUploadRequestDto.java
+++ pochak/src/main/java/com/apps/pochak/post/dto/PostUploadRequestDto.java
@@ -1,5 +1,13 @@
 package com.apps.pochak.post.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class PostUploadRequestDto {
     // testDto
+    private String postImageUrl;
+    private String caption;
+
 }
