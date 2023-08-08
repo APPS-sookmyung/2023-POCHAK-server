59214f109b2179c3d2845584f4af7cde0126cead
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2023-08-08 16:20:55 +0900

    Feat : post api 끝

--- pochak/src/main/java/com/apps/pochak/post/controller/PostController.java
+++ pochak/src/main/java/com/apps/pochak/post/controller/PostController.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
