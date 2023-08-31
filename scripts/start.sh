ba265edf4965bb737f06c2e7cb34f6008e26fdb0
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2023-09-01 00:56:14 +0900

    file app changed

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app/pochak"
+PROJECT_ROOT="/home/ubuntu/pochakapp"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
