287d3e3af690f0dfaac85ee86f095022bba30c4f
HaileyKim <90396746+Haileyyyyyyyy@users.noreply.github.com>
2023-09-15 11:38:05 +0200

    #21 Fix : Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/pochakapp/pochak"
+PROJECT_ROOT="/home/ubuntu/pochakapp"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
@@ -11,7 +11,7 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/pochak/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
