230914377c5fda395329e29909abe0778132256c
HaileyKim <90396746+Haileyyyyyyyy@users.noreply.github.com>
2023-09-15 11:40:10 +0200

    #21 Fix: Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/pochakapp/pochak"
-JAR_FILE="$PROJECT_ROOT/pochak-0.0.1-SNAPSHOT.jar"
+PROJECT_ROOT="/home/ubuntu/pochakapp"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
