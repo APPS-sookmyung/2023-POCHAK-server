cd93b9623c0ffcb0d075c2d79d62a1dce397f263
HaileyKim <90396746+Haileyyyyyyyy@users.noreply.github.com>
2023-09-15 11:16:25 +0200

    #21 Fix: 경로랑 이름 수정 Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/pochakapp"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+PROJECT_ROOT="/home/ubuntu/pochakapp/pochak"
+JAR_FILE="$PROJECT_ROOT/pochak-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
