6e6591280e2af9e05158449997c801a48d8c92b0
Hagoeun <sera1193@naver.com>
2023-10-01 01:06:23 +0900

    Fix: update HomeController

--- pochak/src/main/java/com/apps/pochak/common/controller/HomeController.java
+++ pochak/src/main/java/com/apps/pochak/common/controller/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/index")
     public String home() {
-        return "hi pochak!";
+        return "index";
     }
 }
