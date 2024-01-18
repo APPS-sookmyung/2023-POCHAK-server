04fe030737a23a6748d0ae31a24fbcecc8c58d77
5jisoo <56earls@gmail.com>
2024-01-18 23:58:58 +0900

    Fix: start.sh 프로젝트 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,14 +10,13 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-JAR_NAME=$(ls $PROJECT_ROOT/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$PROJECT_ROOT/build/libs/$JAR_NAME
-cp $JAR_PATH $JAR_FILE
+echo "$TIME_NOW > $JAR_PATH 확인" >> $DEPLOY_LOG
+JAR_NAME=$(ls $PROJECT_ROOT/pochak/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$PROJECT_ROOT/pochak/build/libs/$JAR_NAME
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_FILE --logging.level.org.hibernate.SQL=DEBUG > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_PATH --logging.level.org.hibernate.SQL=DEBUG > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
