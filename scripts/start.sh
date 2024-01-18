801149df02a4d0f682234ca0cc8569b1ce55a77f
5jisoo <56earls@gmail.com>
2024-01-18 23:53:27 +0900

    Fix: update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,11 +11,13 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/pochak/build/libs/*.jar $JAR_FILE
+JAR_NAME=$(ls $PROJECT_ROOT/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$PROJECT_ROOT/build/libs/$JAR_NAME
+cp $JAR_PATH $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_FILE --logging.level.org.hibernate.SQL=DEBUG > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
