198697123fdb9400131a6b20f23d9d155fe48c1b
5jisoo <56earls@gmail.com>
2024-01-22 21:51:02 +0900

    #87 Feat: upload comment api

--- src/main/java/com/apps/pochak/comment/service/CommentService.java
+++ src/main/java/com/apps/pochak/comment/service/CommentService.java
@@ -2,10 +2,10 @@ package com.apps.pochak.comment.service;
 
 import com.apps.pochak.comment.domain.Comment;
 import com.apps.pochak.comment.domain.repository.CommentRepository;
+import com.apps.pochak.comment.dto.request.CommentUploadRequest;
 import com.apps.pochak.comment.dto.response.CommentElements;
 import com.apps.pochak.comment.dto.response.ParentCommentElement;
 import com.apps.pochak.global.apiPayload.exception.GeneralException;
-import com.apps.pochak.global.converter.PageableToPageRequestConverter;
 import com.apps.pochak.login.jwt.JwtService;
 import com.apps.pochak.member.domain.Member;
 import com.apps.pochak.post.domain.Post;
@@ -16,9 +16,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import static com.apps.pochak.global.apiPayload.code.status.ErrorStatus.INVALID_POST_ID;
-import static com.apps.pochak.global.apiPayload.code.status.ErrorStatus.PRIVATE_POST;
-import static com.apps.pochak.global.converter.PageableToPageRequestConverter.*;
+import static com.apps.pochak.global.apiPayload.code.status.ErrorStatus.*;
+import static com.apps.pochak.global.converter.PageableToPageRequestConverter.toPageRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -34,8 +33,7 @@ public class CommentService {
             final Pageable pageable
     ) {
         final Member loginMember = jwtService.getLoginMember();
-        final Post post = postRepository.findPostById(postId);
-        if (post.isPrivate()) throw new GeneralException(PRIVATE_POST);
+        final Post post = findPostById(postId);
         final Page<Comment> commentList = commentRepository.findParentCommentByPost(post, pageable);
         return new CommentElements(loginMember, commentList);
     }
@@ -48,8 +46,28 @@ public class CommentService {
     ) {
         final Comment comment = commentRepository.findParentCommentById(parentCommentId)
                 .orElseThrow(() -> new GeneralException(INVALID_POST_ID));
+        final Post post = findPostById(postId);
+        return new ParentCommentElement(comment, toPageRequest(pageable));
+    }
+
+    @Transactional
+    public void saveComment(Long postId, CommentUploadRequest request) {
+        final Member member = jwtService.getLoginMember();
+        final Post post = findPostById(postId);
+        final Comment comment;
+        if (request.getParentCommentId() == null) {
+            comment = request.toEntity(member, post);
+        } else {
+            final Comment parentComment = commentRepository.findParentCommentById(request.getParentCommentId())
+                    .orElseThrow(() -> new GeneralException(INVALID_COMMENT_ID));
+            comment = request.toEntity(member, post, parentComment);
+        }
+        commentRepository.save(comment);
+    }
+
+    private Post findPostById(Long postId) {
         final Post post = postRepository.findPostById(postId);
         if (post.isPrivate()) throw new GeneralException(PRIVATE_POST);
-        return new ParentCommentElement(comment, toPageRequest(pageable));
+        return post;
     }
 }
